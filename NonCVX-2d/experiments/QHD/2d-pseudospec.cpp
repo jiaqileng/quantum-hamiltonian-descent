2115601dd3dd7737bfb1913314afe64680131152
jli0108 <jli0108@umd.edu>
2022-09-14 19:48:54 -0400

    clean up code to follow directory structure

--- NonCVX-2d/experiments/QHD/2d-pseudospec.cpp
+++ NonCVX-2d/experiments/QHD/2d-pseudospec.cpp
@@ -6,33 +6,27 @@
 #include "to_npy.hpp"
 #include <omp.h>
 #include <matio.h>
-#include <experimental/filesystem>
+#include <filesystem>
 
 #define I complex<double>(0, 1)
 
 using namespace std;
+using namespace std::filesystem;
 using namespace Eigen;
+
 const int RANK = 2;
 const int CAPTURE_FRAME_EVERY = 500;
-const string DATA_DIR = "data2/";
-const string EXPECTED_POTENTIALS_DIR = "expected_potentials2/";
-const string POTENTIALS_DIR = "potentials2/";
+const double GAMMA = 0.01;
+const string HOME_DIR = getenv("HOME");
+const string DATA_DIR = path(HOME_DIR).append("QHD_DATA");
+const string NONCVX_DIR = path(DATA_DIR).append("NonCVX-2d");
 const int NUMBER_OF_FUNCTIONS = 22;
-/*double sqrt_gaussian(const double &x1, const double &x2, const double &stepsize) {
-    double sigma = 1;
-    double x1_mean = 3;
-    double x2_mean = 2;
-    //return (stepsize / (sigma * sqrt(2 * M_PI))) * exp(-((x1 - x1_mean) * (x1 - x1_mean) + (x2 - x2_mean) * (x2 - x2_mean)) / (4 * sigma * sigma));
-    return exp(-((x1 - x1_mean) * (x1 - x1_mean) + (x2 - x2_mean) * (x2 - x2_mean)) / (4 * sigma * sigma));
-}*/
-double potential_function(const double &x1, const double &x2) {
-    return 5 * (x1 * x1 + x2 * x2);
-}
+
 double t_dep_1(const double &t) {
-    return 1 / (1 + 0.01 * t * t);
+    return 1 / (1 + GAMMA * t * t);
 }
 double t_dep_2(const double &t) {
-    return 1 + 0.01 * t * t;
+    return 1 + GAMMA * t * t;
 }
 
 void initialize_psi(ArrayXXcd &y, const double &L, const int &N) {
@@ -51,41 +45,8 @@ void initialize_psi(ArrayXXcd &y, const double &L, const int &N) {
             sum += (y(j, i) * conj(y(j, i))).real();
         }
     }
-
-    // l2 normalization
-    /*double sqrt_sum = sqrt(sum);
-    #pragma omp parallel for private(i,j) schedule(static)
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            y(i, j) /= sqrt_sum;
-        }
-    }*/
-
-    // verify
-    sum = 0;
-    #pragma omp parallel for private(i,j) reduction(+: sum) schedule(static)
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            sum += (y(i, j) * conj(y(i, j))).real();
-        }
-    }
-    printf("sum=%f\n", sum);
 }
-void initialize_potential(ArrayXXd &V, const double &L, const int &N) {
-    double x1, x2;
-    int i, j;
-    double stepsize = 2 * L / N;
 
-    #pragma omp parallel for private(i, j, x1, x2) schedule(static)
-    for (i = 0; i < N; i++) {
-        x1 = -L + i * stepsize;
-        for (j = 0; j < N; j++) {
-            x2 = -L + j * stepsize;
-            // transpose to be consistent with matlab
-            V(j, i) = potential_function(x1, x2);
-        }
-    }
-}
 void load_potential(ArrayXXd &V, const int &N, const int &function_id, double *potential_data) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -116,17 +77,6 @@ void initialize_kinetic_operator(ArrayXXd &y, const double &L, const int &N)
     }
 }
 
-// debugging purposes
-/*void get_prob_sum(const ArrayXXcd &psi0) {
-    int n = psi0.rows();
-    double sum = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            sum += (psi0(i,j) * conj(psi0(i,j))).real();
-        }
-    }
-    printf("sum=%f\n", sum);
-}*/
 void hadamard_product(ArrayXXcd &A, ArrayXXcd &B, ArrayXXcd &out, const int &N) {
     int i, j;
     #pragma omp parallel for private(i,j) schedule(static)
@@ -137,7 +87,7 @@ void hadamard_product(ArrayXXcd &A, ArrayXXcd &B, ArrayXXcd &out, const int &N) 
     }
 }
 
-void pseudospec(const int &num_qubits, const ArrayXXd &V, const double &L, const int &N, ArrayXXcd &psi0,
+void pseudospec(const int &num_qubits, const ArrayXXd &V, const double &L, const int &N, ArrayXXcd &psi,
                 const double &dt, const double &T, const string function_name)
 {
     /*
@@ -145,11 +95,20 @@ void pseudospec(const int &num_qubits, const ArrayXXd &V, const double &L, const
     *    H(t) = -0.5 * tdep(t) * Laplacian + V(x)
     *  defined on [-L,L]^2
     */
+    string function_dir = path(NONCVX_DIR).append(function_name);
+    string wfn_dir = path(function_dir).append(function_name + "_QHD_WFN");
+    if (!filesystem::exists(function_dir)) {
+        filesystem::create_directory(function_dir);
+    }
+    if (!filesystem::exists(wfn_dir)) {
+        filesystem::create_directory(wfn_dir);
+    }
+    // Save wavefunction at time 0
+    write_complex_to_npy(psi, path(wfn_dir).append("psi_0.npy"));
 
     int num_steps = T / dt;
     double t = 0;
     ArrayXd expected_potential(num_steps);
-    // part 2
     ArrayXXd kinetic_operator(N, N);
     ArrayXXcd u(N, N);
     ArrayXXcd psi_new(N, N);
@@ -188,19 +147,16 @@ void pseudospec(const int &num_qubits, const ArrayXXd &V, const double &L, const
     double start, end;
     start = omp_get_wtime();
     for (int step = 0; step < num_steps; step++) {
-        //u = exp(-I * dt * t_dep_2(t) * V) * psi0;
-        
+
         #pragma omp parallel for private(i,j) schedule(static)
         for (i = 0; i < N; i++) {
             for (j = 0; j < N; j++) {
                 temp(i,j) = exp(- I * dt * t_dep_2(t) * V(i,j));
             }
         }
-        hadamard_product(temp, psi0, u, N);
-        // fft2(u1)
+        hadamard_product(temp, psi, u, N);
+
         fftw_execute(p1);
-        // exp(-1i.*dt.*coeffT.*kinetic) .* fft2(u1)
-        //u = exp(- I * dt * t_dep_1(t) * kinetic_operator) * u;
 
         #pragma omp parallel for private(i,j) schedule(static)
         for (i = 0; i < N; i++) {
@@ -218,35 +174,37 @@ void pseudospec(const int &num_qubits, const ArrayXXd &V, const double &L, const
         #pragma omp parallel for private(i,j) schedule(static)
         for (i = 0; i < N; i++) {
             for (j = 0; j < N; j++) {
-                psi0(i,j) = psi_new(i,j) / k;
+                psi(i,j) = psi_new(i,j) / k;
             }
         }
 
-        if (step % CAPTURE_FRAME_EVERY == 0) {
-            string str_t = to_string(t);
-            string padded_t = string(10 - str_t.length(), '0') + str_t;
-            write_complex_to_npy(psi0, DATA_DIR + function_name + "/" + "psi_" + padded_t + ".npy");
+        t += dt;
+
+        if (step > 0 && step % CAPTURE_FRAME_EVERY == 0) {
+            string str_t = to_string((int) (t * 10));
+            string filename = "psi_" + str_t + "e-01.npy";
+            write_complex_to_npy(psi, path(wfn_dir).append(filename));
         }
+
         // Compute expected potential
         double expected_value = 0;
         #pragma omp parallel for private(i,j) reduction(+: expected_value)
         for (i = 0; i < N; i++) {
             for (j = 0; j < N; j++) {
-                expected_value += (psi0(i,j) * conj(psi0(i,j))).real() * V(i,j);
+                expected_value += (psi(i,j) * conj(psi(i,j))).real() * V(i,j);
             }
         }
         expected_potential(step) = expected_value;
-        //expected_potential(step) = ((psi0 * psi0.conjugate()).real() * V).sum();
-        //printf("%f\n", expected_potential(step));
 
         printf("\r%d / %d", step + 1, num_steps);
         fflush(stdout);
-        t += dt;
     }
     end = omp_get_wtime();
     printf("\n");
     printf("Pseudospectral integrator runtime = %.5f s\n", end - start);
-    write_real_to_npy_1d(expected_potential, EXPECTED_POTENTIALS_DIR + "expected_potential_" + function_name + ".npy");
+    
+    string expected_potential_filename = "expected_potential_" + function_name + ".npy";
+    write_real_to_npy_1d(expected_potential, path(function_dir).append(expected_potential_filename));
     fftw_destroy_plan(p1);
     fftw_destroy_plan(p2);
     fftw_cleanup_threads();
@@ -255,8 +213,8 @@ void pseudospec(const int &num_qubits, const ArrayXXd &V, const double &L, const
 int main(int argc, char **argv)
 {
 
-    if (argc != 5 && argc != 6) {
-        perror("Expected arguments: ./2d-pseudospec <L> <num_qubits> <T> <dt> [<potentials_filename>]");
+    if (argc != 6) {
+        perror("Expected arguments: ./2d-pseudospec <L> <num_qubits> <T> <dt> <potentials_filename>");
         exit(EXIT_FAILURE);
     }
     const double L = stod(argv[1]);
@@ -271,91 +229,70 @@ int main(int argc, char **argv)
 
     if (argc == 6) {
         potentials_filename = argv[5];
-        cout << potentials_filename << endl;
+        cout << "Potentials filename: " << potentials_filename << endl;
     }
 
-    // num_qubits can store integers from [-2^num_qubits, 2^num_qubits - 1]
+    // number of qubits for each dimension
     int N = pow(2, num_qubits / 2);
     printf("N=%d\n", N);
 
     double stepsize = 2 * L / N;
     printf("stepsize=%f\n", stepsize);
 
-    printf("max threads: %d\n", omp_get_num_procs());
-    printf("threads: %d\n", omp_get_max_threads());
+    printf("Max threads: %d\n", omp_get_num_procs());
+    printf("Threads: %d\n", omp_get_max_threads());
 
-    if (!experimental::filesystem::is_directory(DATA_DIR)) {
-        experimental::filesystem::create_directory(DATA_DIR);
+    if (!filesystem::exists(DATA_DIR)) {
+        filesystem::create_directory(DATA_DIR);
     }
-    if (!experimental::filesystem::is_directory(POTENTIALS_DIR)) {
-        experimental::filesystem::create_directory(POTENTIALS_DIR);
-    }
-    if (!experimental::filesystem::is_directory(EXPECTED_POTENTIALS_DIR)) {
-        experimental::filesystem::create_directory(EXPECTED_POTENTIALS_DIR);
+    if (!filesystem::exists(NONCVX_DIR)) {
+        filesystem::create_directory(NONCVX_DIR);
     }
     
-    // psi_0
-    ArrayXXcd psi_0(N, N);
+    // psi
+    ArrayXXcd psi(N, N);
 
     // potential V
     ArrayXXd V(N, N);
 
-    if (argc == 5) {
-        if (!experimental::filesystem::is_directory(DATA_DIR + "test" + "/")) {
-            experimental::filesystem::create_directory(DATA_DIR + "test" + "/");
-        }
-        initialize_psi(psi_0, L, N);
-        initialize_potential(V, L, N);
-        write_real_to_npy_2d(V, POTENTIALS_DIR + "potential_test" + ".npy");
-        pseudospec(num_qubits, V, L, N, psi_0, dt, T, "test");
-    } else {
-        // matio
-        mat_t *matfp;
-        matfp = Mat_Open(potentials_filename, MAT_ACC_RDONLY);
-        if (matfp == NULL) {
-            cerr << "Error opening MAT file " << potentials_filename << endl;
-            exit(EXIT_FAILURE);
-        }
-        matvar_t *names_matvar = Mat_VarRead(matfp, "names");
-        if (names_matvar == NULL) {
-            cerr << "Error reading names variable" << endl;
-            exit(EXIT_FAILURE);
-        }
-        matvar_t *name_cell;
-        matvar_t *potentials_matvar;
-        char *function_name;
-        for (int function_id = 0; function_id < NUMBER_OF_FUNCTIONS; function_id++) {
-            
-            initialize_psi(psi_0, L, N);
-            name_cell = Mat_VarGetCell(names_matvar, function_id);
-            function_name = (char*) name_cell->data;
-
-            if (!experimental::filesystem::is_directory(DATA_DIR + function_name + "/")) {
-                experimental::filesystem::create_directory(DATA_DIR + function_name + "/");
-            }
-            
-            printf("Function %d of %d: %s\n", function_id+1, NUMBER_OF_FUNCTIONS, function_name);
-            
-            potentials_matvar = Mat_VarRead(matfp, "potentials");
-            if (potentials_matvar == NULL) {
-                cerr << "Failed to read names or potentials" << endl;
-                exit(EXIT_FAILURE);
-            }
-            load_potential(V, N, function_id, (double*) potentials_matvar->data);
-            if (!experimental::filesystem::is_directory(POTENTIALS_DIR)) {
-                experimental::filesystem::create_directory(POTENTIALS_DIR);
-            }
-            write_real_to_npy_2d(V, POTENTIALS_DIR + "potential_" + function_name + ".npy");
-            
-            pseudospec(num_qubits, V, L, N, psi_0, dt, T, function_name);
-
-        }
-
-        /* Free Mat Variables */
-        Mat_VarFree(potentials_matvar);
-        Mat_VarFree(names_matvar);
-        Mat_Close(matfp);
+    // matio
+    mat_t *matfp;
+    matfp = Mat_Open(potentials_filename, MAT_ACC_RDONLY);
+    if (matfp == NULL) {
+        cerr << "Error opening MAT file " << potentials_filename << endl;
+        exit(EXIT_FAILURE);
     }
+    matvar_t *names_matvar = Mat_VarRead(matfp, "names");
+    if (names_matvar == NULL) {
+        cerr << "Error reading names variable" << endl;
+        exit(EXIT_FAILURE);
+    }
+    matvar_t *name_cell;
+    matvar_t *potentials_matvar;
+    char *function_name;
+    for (int function_id = 0; function_id < NUMBER_OF_FUNCTIONS; function_id++) {
+        
+        initialize_psi(psi, L, N);
+        name_cell = Mat_VarGetCell(names_matvar, function_id);
+        function_name = (char*) name_cell->data;
+        
+        printf("Function %d of %d: %s\n", function_id+1, NUMBER_OF_FUNCTIONS, function_name);
+        
+        potentials_matvar = Mat_VarRead(matfp, "potentials");
+        if (potentials_matvar == NULL) {
+            cerr << "Failed to read names or potentials" << endl;
+            exit(EXIT_FAILURE);
+        }
+        load_potential(V, N, function_id, (double*) potentials_matvar->data);
+        pseudospec(num_qubits, V, L, N, psi, dt, T, function_name);
+
+    }
+
+    /* Free Mat Variables */
+    Mat_VarFree(potentials_matvar);
+    Mat_VarFree(names_matvar);
+    Mat_Close(matfp);
+    
     
     return 0;
 }
--- NonCVX-2d/experiments/QHD/README.md
+++ NonCVX-2d/experiments/QHD/README.md
@@ -10,3 +10,21 @@ Let Jiaqi and Joseph know!
 3. Joseph: re-run the CPP program and check it if works (make sure the output wfn data name matches the dataset Ethan uploaded); 
 4. Joseph: mention the instances that require special parameters in the pseudospectral solver.
 5. Jiaqi: change the qhd wave function name in the fig2 plot script. 
+
+## Usage
+It is recommended to run the program on a Linux distribution with OpenMP installed. The provided Makefile uses g++ to compile the program.
+
+Prerequisites: [make](https://www.gnu.org/software/make/), [Eigen](https://eigen.tuxfamily.org/index.php?title=Main_Page#Download), [FFTW](https://www.fftw.org/download.html), [matio](https://github.com/tbeu/matio)
+
+Assuming the prerequisites are installed, the program can be run by simply running
+```
+make
+./2d-pseudospec <L> <num_qubits> <T> <dt> <potentials_filename>
+```
+In our experiments, we set the parameters as follows:
+```
+./2d-pseudospec 0.5 16 500 0.005 potentials256.mat
+```
+For the results presented in the paper, the QHD parameters are fine-tuned for a few functions. For the Rosenbrock function, we use $\gamma$=0.05 instead of 0.01. The value of $\gamma$ is hardcoded into the program, but one may change it and run `make` again to recompile the program.
+
+For the Holder function, we use dt=0.001 instead of 0.005.
